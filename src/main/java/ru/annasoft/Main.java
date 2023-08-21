f1ba4d3b8a903c5800a20ba42544db84349461e2
denis sergeevich pekshev <pekshev.ds@gmail.com>
2023-08-21 07:30:10 +0300

    refactoring

---
+++ src/main/java/ru/annasoft/Main.java
@@ -0,0 +1,10 @@
+package ru.annasoft;
+
+import ru.annasoft.lesson1.Lesson1;
+public class Main {
+    public static void main(String[] args) {
+
+        Lesson1 lesson1 = new Lesson1();
+        lesson1.sayHello();
+    }
+}
---
+++ src/main/java/ru/annasoft/lesson1/Lesson1.java
@@ -0,0 +1,8 @@
+package ru.annasoft.lesson1;
+
+public class Lesson1 {
+
+    public void sayHello(){
+        System.out.println("Hello, world");
+    }
+}
